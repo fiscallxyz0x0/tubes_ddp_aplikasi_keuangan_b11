8c998d18b4b3dfac0629ff23b4c4721e7fae46ee
HAFIZ FAUZAN SYAFRUDIN <hafiz.fauzan.tif425@polban.ac.id>
2025-11-20 10:15:22 +0700

    Restrukturalisasi Struktur File

--- src/file_handler/laporan/tampilkan_kesimpulan_mahasiswa.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "perhitungan.h"
-
-void tampilkanKesimpulanMahasiswa(double totalPemasukan, double saldo) {
-    const char *kesimpulan = kesimpulanMahasiswa(totalPemasukan, saldo);
-    printf("\nKesimpulan: %s\n", kesimpulan);
-}
